cce7b3f34a20907943598567423caafb66d53f34
angeloomar <asegovia@lucasdiesel.cl>
2023-01-12 16:56:20 -0300

    primer react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListadeTareas from './Components/ListadeTareas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+      <ListadeTareas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ListadeTareas.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ListadeTareas extends Component {
+
+    render() {
+        return (
+            <div>
+                <h4>* Learn React</h4>
+                <h4>* Cimb Mt. Everest</h4>
+                <h4>* Run a marathon</h4>
+                <h4>* Feed the dogs</h4>
+            </div>
+        )
+    }
+};
+
+export default ListadeTareas;
